5a06df5b64f009171fb71c3ec52bcfd3da5129ea
Florian Buetow <mail@florianbuetow.com>
2016-04-23 17:48:48 +0200

    compute the powerset of a set

--- powerset.py
+++ powerset.py
@@ -7,10 +7,10 @@ def powerset(simple_set):
     """
     powerset = [[]]  # include the empty set
     for element in simple_set:  # gradually build the powerset
-        powerset += [subset + list(element) for subset in powerset]
+        powerset = powerset + [subset + list(element) for subset in powerset]
     return powerset
 
-# dest
+# Test
 for n in range(5):
     simple_set = map(lambda x: str(x), range(n))  # convert ints to strings
     power_set = powerset(simple_set)
